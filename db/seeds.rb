3df9a8ee1ead293171898005079fa70966708cec
zzs1020 <mailtozeshi@gmail.com>
2015-09-18 15:32:16 -0400

    seeds change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "zeshi",
+User.create!(name:  "Steve Zheng",
              email: "mailtozeshi@gmail.com",
+             phone: "(978)427-1245",
              password:              "10201121",
              password_confirmation: "10201121",
              admin: 1,
              activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name:  "nonactivated_user",
-             email: "gg@gmail.com",
-             password:              "10201121",
-             password_confirmation: "10201121",
-             activated: true,
-             activated_at: Time.zone.now)
-
-31.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
-
-users = User.order(:created_at).take(6)
-31.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+# User.create!(name:  "nonactivated_user",
+#              email: "gg@gmail.com",
+#              password:              "10201121",
+#              password_confirmation: "10201121",
+#              activated: true,
+#              activated_at: Time.zone.now)
+#
+# 31.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password,
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
+#
+# users = User.order(:created_at).take(6)
+# 31.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
